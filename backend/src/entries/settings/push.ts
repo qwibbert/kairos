e0787f068215d36dfd2b575b6fc907d6d0911e0b
Quinten Libert <libert1quinten@gmail.com>
2025-09-02 13:15:06 +0200

    fix: debug push handlers

--- backend/src/entries/settings/push.ts
+++ backend/src/entries/settings/push.ts
@@ -12,10 +12,10 @@ export default function (e: core.RequestEvent) {
         return e.json(426, ServerErrorFactory.version('settings.push', e.requestInfo().body.id, { request_version: version, server_version: __KAIROS_VERSION__ }))
     }
 
-    console.log(JSON.stringify(e.request));
-
     $app.runInTransaction((tx) => {
         let change_rows: Array<any> = e.requestInfo().body.change_rows;
+
+        $app.logger().error(JSON.stringify(change_rows));
 
         const conflicts = [];
         const event = {
