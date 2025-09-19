2a1965223e39df5f050c58f6e008eb0b63bd8e75
Quinten Libert <libert1quinten@gmail.com>
2025-09-19 15:18:54 +0200

    feat: add language detection

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,12 +1,18 @@
+import universalLanguageDetect from '@unly/universal-language-detector';
 import i18next from 'i18next';
 import en from '../locales/en';
 import nl from '../locales/nl';
 import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async () => {
+	const lang = universalLanguageDetect({
+		supportedLanguages: ['nl', 'en'],
+		fallbackLanguage: 'en'
+	});
+
 	await i18next.init({
-		lng: 'en', // if you're using a language detector, do not define the lng option
 		debug: true,
+		lng: lang,
 		ns: ['account', 'common', 'session', 'settings', 'statistics', 'vines'],
 		resources: {
 			en,
