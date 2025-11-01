022faf5d9fba1bd245bfbe75609492922de62a39
Quinten Libert <libert1quinten@gmail.com>
2025-11-01 18:01:02 +0100

    fix: statistics mode not set when loading component

--- frontend/src/routes/statistics/+page.svelte
+++ frontend/src/routes/statistics/+page.svelte
@@ -1,5 +1,7 @@
-<script>
+<script lang="ts">
 	import StatsUi from "$features/stats/components/stats-ui.svelte";
+
+	let mode: 'GENERAL' | 'VINE' = $state('GENERAL');
 </script>
 
-<StatsUi />
+<StatsUi {mode}/>
